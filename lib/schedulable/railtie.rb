f42173c26acc2463fff48e296e3a925e4f7d6b86
Rafael Nowrotek <mail@benignware.com>
2015-04-10 16:39:35 +0200

    Added locale initializer

--- lib/schedulable/railtie.rb
+++ lib/schedulable/railtie.rb
@@ -1,8 +1,14 @@
 require 'rails'
+require 'i18n'
 module Schedulable
   class Railtie < ::Rails::Railtie
     
     railtie_name :schedulable
+    
+    # requires all dependencies
+    Gem.loaded_specs['schedulable'].dependencies.each do |d|
+     require d.name
+    end
     
     # application configuration initializer
     config.schedulable = ActiveSupport::OrderedOptions.new # enable namespaced configuration in Rails environments
@@ -11,11 +17,26 @@ module Schedulable
       Schedulable.configure do |config|
         
         # copy parameters from application configuration
+        config.form_helper = app.config.schedulable[:form_helper]
         config.max_build_count = app.config.schedulable[:max_build_count]
         config.max_build_period = app.config.schedulable[:max_build_period]
+        
+      end
+    end
+   
+    initializer "schedulable.view" do
+      ActiveSupport.on_load :action_view do
+        include Schedulable::FormHelper
       end
     end
     
+    initializer "schedulable.locales" do
+      
+      Dir[File.join("#{File.dirname(__FILE__)}/../../config/locales/*.yml")].each do |locale|
+        I18n.load_path.unshift(locale)
+      end
+   
+    end
     
     # rake tasks
     rake_tasks do
