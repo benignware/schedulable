ffe25f4dd698973d833995b0335e0c295c40c106
Rafael Nowrotek <mail@benignware.com>
2015-04-10 16:38:20 +0200

    Added form_helper option

--- lib/schedulable.rb
+++ lib/schedulable.rb
@@ -1,16 +1,17 @@
-require 'schedulable/railtie.rb' if defined? ::Rails::Railtie
-require 'schedulable/acts_as_schedulable.rb'
-require 'schedulable/schedule_support.rb'
+require 'schedulable/railtie' if defined? ::Rails::Railtie
+require 'schedulable/acts_as_schedulable'
+require 'schedulable/schedule_support'
+require 'schedulable/form_helper'
 require 'i18n'
 
 module Schedulable
   
   class Config
-    attr_accessor :max_build_count, :max_build_period
+    attr_accessor :max_build_count, :max_build_period, :form_helper
   end
 
   def self.config
-    @@config ||= Config.new
+    @@config||= Config.new
   end
 
   def self.configure
