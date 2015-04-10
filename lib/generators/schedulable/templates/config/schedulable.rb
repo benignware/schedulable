0e6dfa9aafa7b0df76b27d74201f8777aa74b2c7
Rafael Nowrotek <mail@benignware.com>
2015-04-10 16:36:44 +0200

    Changed option names

--- lib/generators/schedulable/templates/config/schedulable.rb
+++ lib/generators/schedulable/templates/config/schedulable.rb
@@ -1,4 +1,7 @@
 Schedulable.configure do |config|
-  config.max_build_count = 0
-  config.max_build_period = 1.year
+  config.max_count = 0
+  config.max_until = 1.year
+  config.form_helper = {
+    style: :default
+  }
 end
