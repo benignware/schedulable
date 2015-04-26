e4c3d8241a8d6446c1435a71924d164dd0b208c8
Rafael Nowrotek <mail@benignware.com>
2015-04-26 14:19:30 +0200

    Changed locale of dummy app to 'en'

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -17,7 +17,10 @@ module Dummy
     config.time_zone = 'Europe/Berlin'
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
+    
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+    
   end
 end
 
