6daf9d2aedd5f38c51af764515ea9546831e13f0
Charanya Nagarajan <charanya.nagarajan@tcs.com>
2016-04-12 14:35:56 +0530

    Fixes the timezone issue, with event occurrences of recurring events.

--- lib/schedulable/schedule.rb
+++ lib/schedulable/schedule.rb
@@ -1,66 +1,68 @@
 module Schedulable
   module Model
     class Schedule  < ActiveRecord::Base
-      
+
       serialize :day
       serialize :day_of_week, Hash
-    
+
       belongs_to :schedulable, polymorphic: true
-    
+
       after_initialize :update_schedule
       before_save :update_schedule
-      
+
       validates_presence_of :rule
       validates_presence_of :time
       validates_presence_of :date, if: Proc.new { |schedule| schedule.rule == 'singular' }
       validate :validate_day, if: Proc.new { |schedule| schedule.rule == 'weekly' }
       validate :validate_day_of_week, if: Proc.new { |schedule| schedule.rule == 'monthly' }
-      
+
       def to_icecube
         return @schedule
       end
-      
+
       def to_s
         return @schedule.to_s
       end
-      
+
       def method_missing(meth, *args, &block)
         if @schedule.present? && @schedule.respond_to?(meth)
           @schedule.send(meth, *args, &block)
         end
       end
-      
+
       def self.param_names
         [:id, :date, :time, :rule, :until, :count, :interval, day: [], day_of_week: [monday: [], tuesday: [], wednesday: [], thursday: [], friday: [], saturday: [], sunday: []]]
       end
-    
+
       def update_schedule()
-        
+
         self.rule||= "singular"
         self.interval||= 1
         self.count||= 0
 
-        time = Date.today.to_time 
-        if self.time.present? 
-          time = time + self.time.seconds_since_midnight.seconds   
+        time = Date.today.to_time(:utc)
+        if self.time.present?
+          time = time + self.time.seconds_since_midnight.seconds
         end
+        time_string = time.strftime("%d-%m-%Y %I:%M %p")
+        time = Time.zone.parse(time_string)
 
         @schedule = IceCube::Schedule.new(time)
-        
+
         if self.rule && self.rule != 'singular'
-          
+
           self.interval = self.interval.present? ? self.interval.to_i : 1
-          
+
           rule = IceCube::Rule.send("#{self.rule}", self.interval)
-          
+
           if self.until
             rule.until(self.until)
           end
-          
+
           if self.count && self.count.to_i > 0
             rule.count(self.count.to_i)
           end
-        
+
           if self.day
             days = self.day.reject(&:empty?)
             if self.rule == 'weekly'
@@ -77,18 +79,18 @@ module Schedulable
           end
           @schedule.add_recurrence_rule(rule)
         end
-        
+
       end
-      
+
       private
-      
+
       def validate_day
         day.reject! { |c| c.empty? }
         if !day.any?
           errors.add(:day, :empty)
         end
       end
-      
+
       def validate_day_of_week
         any = false
         day_of_week.each { |key, value|
