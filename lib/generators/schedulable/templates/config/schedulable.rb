784a228dd04c979a3b8e68d8d549b42dea90dc39
Rafael Nowrotek <mail@benignware.com>
2016-04-14 20:41:23 +0200

    Fixed config generator template is not up to date

--- lib/generators/schedulable/templates/config/schedulable.rb
+++ lib/generators/schedulable/templates/config/schedulable.rb
@@ -1,6 +1,6 @@
 Schedulable.configure do |config|
-  config.max_count = 0
-  config.max_until = 1.year
+  config.max_build_count = 0
+  config.max_build_period = 1.year
   config.simple_form = {
   }
   config.form_helper = {
